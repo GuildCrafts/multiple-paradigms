6f24907ad5a1aabb189261acffdf5cc9f7886c7e
Max Antinone <mantinone@gmail.com>
2017-01-18 18:09:27 -0800

    Starting our oo method

--- javascript/spec/test.js
+++ javascript/spec/test.js
@@ -3,6 +3,29 @@ import oo from '../object_oriented.js'
 import functional from '../functional.js'
 import imperative from '../imperative.js'
 
+const tags = {
+  root: {
+    list: ['ol', 'ul']
+      , header: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+      , blockQuote: 'blockquote'
+      , hr: 'hr'
+      , paragraph: 'p'
+      , codeBlock: 'pre'
+  }
+    , formatting: {
+      link: 'a'
+        , bold: 'strong'
+        , italic: 'em',
+        , boldItalic: {
+            bold: 'strong'
+          , italic: 'em'
+        }
+    }
+    , terminal: {
+      image:
+    }
+}
+
 const test = (input, output) => {
   const paradigms = [
     ['Object Oriented', oo]
@@ -10,20 +33,20 @@ const test = (input, output) => {
     , ['Imperative', imperative]
   ]
 
-  for (let i in paradigms) {
-    const [name, fn] = paradigms[i]
+  paradigms.map( paradigm => {
+    const [name, fn] = paradigm
     it(name, () => {
       expect(fn(input)).to.equal(output)
     })
-  }
+  })
 }
 
-const nest = (element) => {
+const testNested = (element) => {
   const [name, tag] = element
 
   describe(`Within ${name} / ${tag}: `, () => {
     const fileName = name
-    
+
     const input = 'test' // use FS to read input file into single string
     const output = fileName // use FS to read output file into single string
 
@@ -35,32 +58,36 @@ describe('Multiple Paradigms', () => {
   'use strict'
 
   it('A function exists for each paradigm', () => {
-    expect(oo).to.be.a('function')
-    expect(functional).to.be.a('function')
-    expect(imperative).to.be.a('function')
+    const paradigms = [oo, functional, imperative]
+    paradigms.map( x => expect(x).to.be.a('function') )
+  })
+
+  it('Each function returns valid html', () => {
+    test('', '<div></div>')
   })
 
 
-  describe('\nCorrectly parses the following structures:', () => {
+})
 
-    describe('\\n\\n + text + \\n\\n -> <p>', () => {
+describe('\nCorrectly parses root structures:', () => {
 
-      const input = '\n\ntest\n\n'
-      const output = '<p>test</p>'
-  
-      test(input, output)
-    })
+  describe('\\n\\n + text + \\n\\n -> <p>', () => {
 
-    describe('# + text + \\n -> <h1>', () => {
+    const input = '\n\ntest\n\n'
+    const output = '<p>test</p>'
 
-      const input = '#test\n'
-      const output = '<h1>test</h1>'
-  
-      test(input, output)
-    })
+    test(input, output)
   })
 
-  describe('\nCorrectly parses nested structures: ', () => {
-    nest(['paragraph', '<p>'])
+  describe('# + text + \\n -> <h1>', () => {
+
+    const input = '#test\n'
+    const output = '<h1>test</h1>'
+
+    test(input, output)
   })
 })
+
+describe('\nCorrectly parses nested structures: ', () => {
+  testNested(['paragraph', '<p>'])
+  })
